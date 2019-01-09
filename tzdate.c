9e92700b173a0ed8b1d8eeb2e20e3747ae86b3f5
Carlos Ortiz <chrono.dark@gmail.com>
2019-01-09 17:05:29 -0600

    Remove useless files

--- tzdate.c
+++ tzdate.c
@@ -2,6 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+/**
+ * Returns the tm structure base on the Given Timezone and time structre.
+ * if the timezone can't be match will use UTC.
+ *
+ * **/
 struct tm  *getTime(char* tz, time_t *rawTime){
     setenv("TZ", tz, 1);
     tzset();
@@ -10,15 +15,24 @@ struct tm  *getTime(char* tz, time_t *rawTime){
     return result;
 }
 
+/**
+ * Main function.
+ * **/
 int main (int argc, char **argv){
+    if (argc==1){
+        puts("Time zone is required");
+        return 3;
+    }
     time_t rawTime = time(NULL);
+    char buffer[80];
     if(rawTime == -1){
         printf("Unable to get time");
         return 1;
     }
-    struct tm *loctime = getTime("Asia/Hong_Kong", &rawTime);
-    printf("Hong Kong %02d:%02d:%02d\n",loctime->tm_hour, loctime->tm_min, loctime->tm_sec);
-    loctim = getTime("EST", &rawTime);
-    printf("EST %02d:%02d:%02d\n",loctime->tm_hour, loctime->tm_min, loctime->tm_sec);
+    for(int i = 1; i<argc; i++){
+        struct tm *loctime = getTime(argv[i], &rawTime);
+        strftime(buffer,80,"%c", loctime);
+        printf("%s %s\n",argv[i], buffer);
+    }
     return 0;
 }
